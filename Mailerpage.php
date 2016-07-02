d5351afb10557b707e4986c1518e811af0cb87ba
stvnvndnbrgh <stvnvndnbrgh@gmail.com>
2016-07-02 09:05:02 +0200

    Merge origin/master
    Conflicts:
    	Presentation/nieuweBestellingForm.php

--- Mailerpage.php
+++ Mailerpage.php
@@ -16,7 +16,7 @@ $m->SMTPSecure = 'ssl';
 $m->Port = 465;
 
 $m->From = 'stvnvndnbrgh@gmail.com';
-$m->FromName = 'Bandagisterie Heverelee';
+$m->FromName = 'Bandagisterie Heverlee';
 $m->addReplyTo('info@bandagisterieheverlee.be','Reply address');
 $m->addAddress('stvnvndnbrgh@gmail.com', 'Steven van den Berghe');
 
@@ -28,4 +28,4 @@ $m->Subject = 'Bestelling Bandagisterie Heverlee';
 $m->Body = '<p>Dit is een bestelling</p><br /><p>Met vriendelijke groeten,<p>';
 $m->AltBody = 'Dit is een bestelling';
 
-//var_dump($m->send());
+var_dump($m->send());
