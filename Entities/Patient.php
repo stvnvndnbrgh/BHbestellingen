29b59cea5bb8d041e0c3b994f03903b779ae1017
stvnvndnbrgh <stvnvndnbrgh@gmail.com>
2016-08-22 17:06:47 +0200

    Merge origin/master
    Conflicts:
    	Presentation/nieuweBestellingForm.php

---
+++ Entities/Patient.php
@@ -0,0 +1,12 @@
+<?php
+//Entities/Patient.php
+
+class Patient extends Klant{
+    
+    private static $mapId = array();
+    
+    private $id;
+    private $mut_id;
+    private $rijksregisternr;
+}
+
--- fpdfTest.php
+++ fpdfTest.php
@@ -1,12 +1,34 @@
 <?php
-require_once('fpdf.php');
-require_once('fpdi.php');
+require_once 'Vendor/vendor/autoload.php';
 
 // initiate FPDI
 $pdf = new FPDI();
 // add a page
 $pdf->AddPage();
+// set the source file
+$pdf->setSourceFile('img/Riziv_steunzolen_blanco.pdf');
+// import page 1
+$tplIdx = $pdf->importPage(1);
+// use the imported page and place it at position 10,10 with a width of 100 mm
+$pdf->useTemplate($tplIdx, 0, 0, 210);
 
+// now write some text above the imported page
+// de naam van de patient zetten
+$pdf->SetFont('Helvetica', '', 7);
+$pdf->SetTextColor(0, 0, 0);
+$pdf->SetXY(63, 34.6);
+$pdf->Write(0, 'Steven Van den Berghe');
+$pdf->SetXY(63, 59.6);
+$pdf->Write(0, 'Steven Van den Berghe');
+// ziekenfonds zetten
+$pdf->SetXY(40, 37.6);
+$pdf->Write(0, 'Christelijk ziekenfonds - Sint-Pietersbond (108)');
+// rijksregisternummer zetten
+$pdf->SetXY(33, 40.6);
+$pdf->Write(0, '750902-215-05');
+
+
+$pdf->Output();
 ?>
 
 
